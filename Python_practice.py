72cc983c088572eab35111d1ea9099110306d981
okc12nz <harrymorton@rocketmail.com>
2021-05-13 07:12:09 -0700

    Playing around with the .py file

--- Python_practice.py
+++ Python_practice.py
@@ -1,6 +1,7 @@
-counties = {}
-counties["Santa Clara"] = "Cool As"
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
 
-voting = []
-voting.append({"county":"Arapahoe", "registered_voters": 422829})
-print(voting[0]['county'])
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
