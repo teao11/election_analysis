a0da960b6eb550f9d423150f2f43bacbd16f422d
okc12nz <harrymorton@rocketmail.com>
2021-05-16 09:23:26 -0700

    RW stuff

--- PyPoll.py
+++ PyPoll.py
@@ -1,11 +1,20 @@
-#In this project, our final Python script will need to be able to deliver the following information when the script is run: 
-#Total number of votes cast
-#A complete list of candidates who received votes
-#Total number of votes each candidate received
-#Percentage of votes each candidate won
-#The winner of the election based on popular vote
+import csv
+import os
 
-import datetime as dt
-now = dt.datetime.now()
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources","election_results.csv")
 
-print("The time right now is ", now)
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("Analysis", "election_analysis.txt")
+
+# Open the election results and read the file
+with open(file_to_load,'r') as election_data:
+
+	# Read the file
+	file_reader = csv.reader(election_data)
+
+	# Print the header row
+	headers = next(file_reader)
+	print(headers)
+
+	
